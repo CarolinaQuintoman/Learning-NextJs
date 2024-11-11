d002aecad716090a77b47bddff565aa0d89833fb
CarolinaQuintoman <quintoman.carolina@gmail.com>
2024-11-11 16:52:42 -0300

    firt commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// importo la fuente, mi font principal
+import { Inter, Lusitana } from 'next/font/google';
+// especificacion del subconjunto, seria latin
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400','700'], subsets: ['latin'] })
